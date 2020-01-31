800d3193d512f1d85591a0987fd82e997c37fdd0
Rachel Humble <rachelhumble@me.com>
2020-01-31 00:49:37 -0500

    Secondary commit pushing local storage feature

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # daily_scheduler
+
+## User Story
+
+AS AN employee with a busy schedule
+
+I WANT to add important events to a daily planner
+
+SO THAT I can manage my time effectively 
+
+## Business Context
+
+Poor time management can result in missed meetings and deadlines or create the appearance of unprofessionalism. A daily planner allows employees to see their day at a glance, schedule time effectively, and improve productivity. 
+
+## Minimum Requirements
+
+* Functional, deployed application.
+
+* GitHub repository with a unique name and a README describing project.
+
+* The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).
+
+* Each timeblock contains an input field and save button.
+
+* Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed.
+
+* The current day is displayed at the top of the calendar.
+
+* Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+
+```
+GIVEN that an employee adds events to a specific hour in a calendar
+
+WHEN the employee clicks the save button
+
+THEN events are saved in the timeblock for that hour
--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,101 @@ document.getElementById("date").innerHTML = d.toDateString();
 
 var saveBtn = $(".btn");
 
-saveBtn.on("click", function() {
-    console.log("Save clicked");
-    var event = $(".form-control");
-    localStorage.setItem("event", event);
+//Set saved events
+//9am
+var nineEvent = localStorage.getItem("9am");
+var firstEvent = document.getElementById("9event");
+firstEvent.value = nineEvent;
+//10a
+var tenEvent = localStorage.getItem("10am");
+var secondEvent = document.getElementById("10event");
+secondEvent.value = tenEvent;
+//11a
+var elevenEvent = localStorage.getItem("11am");
+var thirdEvent = document.getElementById("11event");
+thirdEvent.value = elevenEvent;
+//12p
+var twelveEvent = localStorage.getItem("12pm");
+var fourthEvent = document.getElementById("12event");
+fourthEvent.value = twelveEvent;
+//1p
+var oneEvent = localStorage.getItem("1pm");
+var fifthEvent = document.getElementById("13event");
+fifthEvent.value = oneEvent;
+//2p
+var twoEvent = localStorage.getItem("2pm");
+var sixthEvent = document.getElementById("14event");
+sixthEvent.value = twoEvent;
+//3p
+var threeEvent = localStorage.getItem("3pm");
+var seventhEvent = document.getElementById("15event");
+seventhEvent.value = threeEvent;
+//4p
+var fourEvent = localStorage.getItem("4pm");
+var eighthEvent = document.getElementById("16event");
+eighthEvent.value = fourEvent;
+//5p
+var fiveEvent = localStorage.getItem("5pm");
+var ninthEvent = document.getElementById("17event");
+ninthEvent.value = fiveEvent;
+
+saveBtn.on("click", function(e) {
+    e.preventDefault();
+    //9a
+    var nine = $("#9event").val();
+    localStorage.setItem("9am", nine);
+    //10a
+    var ten = $("#10event").val();
+    localStorage.setItem("10am", ten);
+    //11a
+    var eleven = $("#11event").val();
+    localStorage.setItem("11am", eleven);
+    //12p
+    var twelve = $("#12event").val();
+    localStorage.setItem("12pm", twelve);
+    //1p
+    var one = $("#13event").val();
+    localStorage.setItem("1pm", one);
+    //2p
+    var two = $("#14event").val();
+    localStorage.setItem("2pm", two);
+    //3p
+    var three = $("#15event").val();
+    localStorage.setItem("3pm", three);
+    //4p
+    var four = $("#16event").val();
+    localStorage.setItem("4pm", four);
+    //5p
+    var five = $("#17event").val();
+    localStorage.setItem("5pm", five);
 });
 
+// const m = moment();
+
+// var iscurrentDate = startTime.isSame(new Date(), "hour");
+// if(iscurrentDate) {
+//     console.log("present");
+// }
+
+   
+// var hour = moment().format("h");
+// console.log(hour);
+// var start = moment().hour(9);
+// var diff = start.diff(hour);
+// console.log(diff);
+
+// if(diff < 0) {
+//     console.log("past");
+// } else if(diff > 0) {
+//     console.log("future");
+// } else {
+//     console.log("current");
+// }
 
 
+
+
+
+// console.log(moment().format("h"));
+// console.log(moment().startOf('day').fromNow());
+// console.log(moment().endOf('day').fromNow());
